4da15a7b6f2e468487df0d65e2a00f40728a4cd0
femi <olufemidokun@hotmail.co.uk>
2018-04-07 21:00:52 +0100

    phone list displaying

--- phonebook/contact/views.py
+++ phonebook/contact/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
 # Create your views here.
+from .models import Contact
+from django.views.generic import ListView
+
+class ContactsListView(ListView):
+    template_name = "contact/list.html"
+
+    def get_queryset(self):
+        all_contacts = Contact.objects.all()
+
+        return all_contacts
